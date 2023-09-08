dea102fb765857bb8498ae6c54fdb2730799bc63
jacktone-wambui <jacktoneabongo@gmail.com>
2023-09-08 16:27:52 +0300

    first commit

---
+++ api.php
@@ -0,0 +1,35 @@
+<?php
+
+$slackName = $_GET['slack_name'] ?? '';
+$track = $_GET['track'] ?? '';
+
+// Get current day of the week
+$currentDay = date('l');
+
+// Get current UTC time with validation of +/-2 minutes
+$utcTime = new DateTime('now', new DateTimeZone('UTC'));
+$currentTime = new DateTime('now');
+$timeWindow = $currentTime->diff($utcTime)->format('%i');
+if ($timeWindow > 2) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Invalid UTC time']);
+    exit;
+}
+
+// GitHub URLs
+$githubFileUrl = 'https://github.com/username/repo/blob/main/file_name.ext';
+$githubRepoUrl = 'https://github.com/username/repo';
+
+// Response JSON
+$response = [
+    'slack_name' => $slackName,
+    'current_day' => $currentDay,
+    'utc_time' => $utcTime->format('Y-m-d\TH:i:s\Z'),
+    'track' => $track,
+    'github_file_url' => $githubFileUrl,
+    'github_repo_url' => $githubRepoUrl,
+    'status_code' => 200,
+];
+
+header('Content-Type: application/json');
+echo json_encode($response);
